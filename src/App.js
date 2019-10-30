ef0f155f3a5bfe0aa2a251a19c89cfe208640f3c
burn-c <chopcsom@gmail.com>
2019-10-29 23:21:21 -0300

    Config: Import->Image

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import './App.css';
+
+
+import profile from './assets/profile.png';
+
 function App() {
-    return <h1>Hello Rocketseat</h1>
+    return <img src={profile}/>
 }
 
 export default App;
