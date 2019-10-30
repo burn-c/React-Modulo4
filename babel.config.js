070c3677393d790e10dfdf6dc2bd9ab9552288ea
burn-c <chopcsom@gmail.com>
2019-10-29 22:55:30 -0300

    Install-> BabelLoader/DevServer

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    presets: [
+        "@babel/preset-env",    // Compatibilidade do JS que o navegador ainda não interpreta
+        "@babel/preset-react"  //  Compatibiliade do React que o navegador ainda não interpreta
+    ],
+};
