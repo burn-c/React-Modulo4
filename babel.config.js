aacd7499bfa6c4fa7f39d89a13bafe88d6c16ac8
burn-c <chopcsom@gmail.com>
2019-10-30 22:01:50 -0300

    Config: ClassComponents

--- babel.config.js
+++ babel.config.js
@@ -3,4 +3,7 @@ module.exports = {
         "@babel/preset-env",    // Compatibilidade do JS que o navegador ainda não interpreta
         "@babel/preset-react"  //  Compatibiliade do React que o navegador ainda não interpreta
     ],
+    plugins: [
+        '@babel/plugin-proposal-class-properties'
+    ]
 };
