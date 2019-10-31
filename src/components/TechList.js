0dd0ea765d628b551c1fb8b56864e8add691f1ed
burn-c <chopcsom@gmail.com>
2019-10-30 22:35:52 -0300

    Config: Insert

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -2,19 +2,40 @@ import React, { Component } from 'react';
 
 class TechList extends Component {
     state = {
-        tech: [
+        newTech: '',
+        techs: [
         'Node.js',
         'ReactJS',
         'React Native'
         ]
+    };
+
+    handleInputChange = e => {
+        this.setState({ newTech: e.target.value});
     }
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        this.setState({ 
+            techs: [...this.state.techs, this.state.newTech],
+            newTech: ''
+        });
+    }
+
     render() {
         return (
+            <form onSubmit={this.handleSubmit}>
             <ul>
-                <li>Node.js</li>
-                <li>ReactJS</li>
-                <li>React Native</li>
+                {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
             </ul>
+                <input 
+                type="text" 
+                onChange={this.handleInputChange} 
+                value={this.state.newTech} 
+                />
+                <button type="submit">Enviar</button>
+                </form>
         );
     }
 }
